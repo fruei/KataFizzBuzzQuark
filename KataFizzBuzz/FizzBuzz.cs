210d25945c528c035250cc7a6e9e085958c654f9
Emanuel Jara <emanueljara96@gmail.com>
2021-10-13 12:56:46 -0300

    Upload project

---
+++ KataFizzBuzz/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace KataFizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string NumToFizzBuzz(int number)
+        {
+            if (!DivisibleBy3OrContains3(number)&&!DivisibleBy5OrContains5(number)) return number.ToString();
+            string result = "";
+            if (DivisibleBy3OrContains3(number)) result += "Fizz";
+            if (DivisibleBy5OrContains5(number)) result += "Buzz";
+            return result;
+        }
+
+        bool DivisibleBy3OrContains3(int number)
+        {
+            return number % 3 == 0 || number.ToString().Contains('3') ? true : false;
+        }
+        bool DivisibleBy5OrContains5(int number)
+        {
+            return number % 5 == 0 || number.ToString().Contains('5') ? true : false;
+        }
+
+        public string[] NumListToFizzBuzzNumberList(int[] numbersList)
+        {
+            string[] result = new string[numbersList.Length];
+            for (int i = 0; i < numbersList.Length; i++)
+            {
+                result[i] = NumToFizzBuzz(numbersList[i]);
+            }
+            return result;
+        }
+    }
+}
---
+++ KataFizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,49 @@
+using KataFizzBuzz;
+using System;
+using Xunit;
+
+namespace KataFizzBuzzTest
+{
+    public class FizzBuzzTest
+    {
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(3, "Fizz")]
+        [InlineData(4, "4")]
+        [InlineData(5, "Buzz")]
+        [InlineData(6, "Fizz")]
+        [InlineData(13, "Fizz")]
+        [InlineData(15, "FizzBuzz")]
+        [InlineData(55, "Buzz")]
+        [InlineData(45, "FizzBuzz")]
+        [InlineData(67, "67")]
+        public void NumToFizzBuzz_Number_FizzBuzzStringOrNumber(int number, string expected)
+        {
+            /// Arrange
+            FizzBuzz _fizzBuzz = new FizzBuzz();
+            /// Act
+            string result = _fizzBuzz.NumToFizzBuzz(number);
+            /// Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1,2,3 }, new string[] { "1", "2", "Fizz" })]
+        [InlineData(new int[] { 4, 5, 6, 7, 8, 9, 10 },
+            new string[] { "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz" })]
+        [InlineData(
+            new int[] { 11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32 },
+            new string[] { "11", "Fizz", "Fizz", "14", "FizzBuzz", "16", "17",
+                "Fizz", "19", "Buzz", "Fizz", "22", "Fizz", "Fizz", "Buzz",
+                "26", "Fizz", "28","29", "FizzBuzz", "Fizz", "Fizz" })]
+        public void NumListToFizzBuzzNumberList_NumbersList_FizzBuzzNumberPopulatedList(int[] numbersList, string[] expected)
+        {
+            /// Arrange
+            FizzBuzz _fizzBuzz = new FizzBuzz();
+            /// Act
+            string[] result = _fizzBuzz.NumListToFizzBuzzNumberList(numbersList);
+            /// Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
